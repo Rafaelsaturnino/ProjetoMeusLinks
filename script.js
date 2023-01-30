c2b3ad37a37f70963c31c346730231d9fb4d0733
Rafael <rafaelsaturnino15@hotmail.com>
2023-01-30 15:28:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // if (html.classList.contains('Light')) {
+  //   html.classList.remove('Light')
+  // } else {
+  //   html.classList.add('Light')
+  // }
+
+  // pegar a tag img
+  // substituir a imagem
+  // se tiver light mode, adicionar a imagem light
+  // se tiver sem light mode, manter a imagem normal
+}
